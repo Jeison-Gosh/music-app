c719ff0ef8a725e5df8c1c78cd0c46bc31e9ca8c
Jeison-Gosh <84155560+Jeison-Gosh@users.noreply.github.com>
2023-07-09 14:53:05 -0500

    09/07/2023 propierties y variables

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
@@ -31,11 +31,13 @@ export class IntroPage implements OnInit {
       title: "Perrito",
       img: "https://www.informador.mx/__export/1595406095362/sites/elinformador/img/2020/07/22/perrito_crop1595406075803.jpg_1902800913.jpg",
       icon: "beer-outline",
+      description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
     }, 
     {
       title: "Pajaro",
       img: "https://demascotas.info/wp-content/uploads/2019/03/Cacatua_alba_-upper_body-8a.jpg",
       icon: "beer-outline",
+      description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
     }
   ]
   constructor(private router: Router) { }
